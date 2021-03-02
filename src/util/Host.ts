30ac9de282e6a85f6e8b0bca261f55f93cb08a27
AntonioAlmeidaRego <antonio.alm_pdf@hotmail.com>
2021-03-02 17:19:05 -0300

    Adicionando a variavel de ambiente HOST_ENV

--- src/util/Host.ts
+++ src/util/Host.ts
@@ -1,8 +1,12 @@
 /*Class Util para reutilizar o Host default do consumo*/
 
+// Carrega as variaveis de ambiente
+require("dotenv").load();
+
+
 class Host {
 
-    private _get = "http://192.168.1.10:8080" //IP Antônio
+    private _get = process.env.HOST_ENV //IP Antônio
     //private _get = "http://192.168.1.127:8080" //IP Moisés
 
     /*getters and setters*/
