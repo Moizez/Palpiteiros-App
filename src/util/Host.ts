ba72ba75e8c2b6933fccfa220cfa2af53b67a260
AntonioAlmeidaRego <antonio.alm_pdf@hotmail.com>
2021-03-02 18:07:46 -0300

    corrigindo...

--- src/util/Host.ts
+++ src/util/Host.ts
@@ -1,17 +1,13 @@
 /*Class Util para reutilizar o Host default do consumo*/
 
-// Carrega as variaveis de ambiente
-require("dotenv").load();
-
-
 class Host {
 
-    private _get = process.env.HOST_ENV //IP Antônio
+    private _get = 'http://192.168.1.10:8080' //IP Antônio
     //private _get = "http://192.168.1.127:8080" //IP Moisés
 
     /*getters and setters*/
 
-    get get(): string {
+    get get(): string { 
         return this._get;
     }
 
