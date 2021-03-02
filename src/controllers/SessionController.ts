88f3a700c657f73123ddcd1cc6072518e52c89b5
AntonioAlmeidaRego <antonio.alm_pdf@hotmail.com>
2021-03-02 14:20:51 -0300

    Adicionando a classe SessionController.ts

---
+++ src/controllers/SessionController.ts
@@ -0,0 +1,38 @@
+import AsyncStorage from '@react-native-community/async-storage'
+
+class SessionController {
+    public async create (key: string, object: object): Promise<object | []>{
+        const isCheck = await this.checkSession(key);
+
+        if (isCheck){
+            await this.remove(key);
+        }
+
+        await AsyncStorage.setItem(key,
+            JSON.stringify(object))
+
+        return this.get(key);
+    }
+
+    public async get (key): Promise<object | []> {
+        return await JSON.parse(await AsyncStorage.getItem(key)) || [];
+    };
+
+    private async checkSession(key): Promise<boolean>{
+        const session = await this.get(key);
+        if (Object.keys(session).length > 0){
+            return true;
+        }
+        return false;
+    }
+
+    public async remove (key): Promise<void>{
+        await AsyncStorage.removeItem(key);
+    }
+
+    public async clear (): Promise<void>{
+        await AsyncStorage.clear();
+    }
+}
+
+export default new SessionController();
